7b704a648aac80cbb8d37066ccf627782742d757
Sergej Jurecko <sergej.jurecko@wpn.tv>
2014-01-21 18:10:14 +0100

    rebar.config change

--- startdev.sh
+++ startdev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec erl -pa ebin deps/*/ebin apps/myactor/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
+exec erl -pa ebin deps/*/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
