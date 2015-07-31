487a4f76a69541cba40c5118548e0992e15427cb
Sergej Jurečko <sergej.jurecko@wpn.tv>
2015-07-31 13:32:35 +0200

    forgot flag

--- startdev.sh
+++ startdev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 case $1 in
     valgrind)
-        export VALGRIND_MISC_FLAGS="-v --trace-children=yes  --tool=memcheck  --suppressions=../otp/erts/emulator/valgrind/suppress.standard --show-possibly-lost=no"
+        export VALGRIND_MISC_FLAGS="-v --fair-sched=yes --trace-children=yes  --tool=memcheck  --suppressions=../otp/erts/emulator/valgrind/suppress.standard --show-possibly-lost=no"
         exec ../otp/bin/cerl -valgrind -pa ebin deps/*/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
         ;;
     debug)
