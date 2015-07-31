41ac6aec1d8a766b959e0c4b504fc4ed5bdbb20c
Sergej Jurečko <sergej.jurecko@wpn.tv>
2015-07-31 13:30:16 +0200

    start with valgrind or debug for dev

--- startdev.sh
+++ startdev.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-exec erl -pa ebin deps/*/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
+case $1 in
+    valgrind)
+        export VALGRIND_MISC_FLAGS="-v --trace-children=yes  --tool=memcheck  --suppressions=../otp/erts/emulator/valgrind/suppress.standard --show-possibly-lost=no"
+        exec ../otp/bin/cerl -valgrind -pa ebin deps/*/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
+        ;;
+    debug)
+        exec ../otp/bin/cerl -debug -pa ebin deps/*/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
+	;;
+    *)
+        exec erl -pa ebin deps/*/ebin -config etc/app.config -s actordb_core -args_file etc/vm.args
+esac
+
+
